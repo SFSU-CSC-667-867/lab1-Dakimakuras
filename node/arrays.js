c278f9e3f1c6e0542d78927df6385527456fc601
Juan Valdez <jdvaldez@mail.sfsu.edu>
2019-02-06 20:37:56 -0800

    did array.js and messed around with some functions

--- node/arrays.js
+++ node/arrays.js
@@ -15,15 +15,24 @@ array[1] = 'hello';
 // for practice
 
 // use forEach
-
+const arrayTest = [9, 3, 4];
+console.log(arrayTest);
+arrayTest.forEach((n) => {
+    console.log(n);
+});
 // use map
-
+const newArr=arrayTest.map((i) => i+1); //(i => i+1) when only 1 number
+console.log(newArr);
 // use pop
-
+newArr.pop();
+console.log(newArr);
 // use push
-
+newArr.push(18);
+console.log(newArr);
 // use shift
-
+newArr.shift();
+console.log(newArr);
 // use unshift
-
+newArr.unshift(64);
+console.log(newArr);
 // use filter
