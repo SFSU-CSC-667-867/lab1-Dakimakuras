ca378b4937ec43ef50dbb09e24748cae4908760e
Juan Valdez <jdvaldez@mail.sfsu.edu>
2019-02-06 21:07:04 -0800

    Messed around with more files given.

--- node/nextTick.js
+++ node/nextTick.js
@@ -1,4 +1,4 @@
 setImmediate(() => console.log('Now'));
-process.nextTick(() => console.log('Hello'));
-
+process.nextTick(() => console.log('Hello')); //last of current pull cycle
+setImmediate(() => console.log('Now 2')); //sets to top of next pull cycle
 console.log('world');
--- node/promises.js
+++ node/promises.js
@@ -5,6 +5,9 @@ const promiseExample = () => {
 };
 
 promiseExample()
-  .then((res) => console.log(res));
+  .then((res) => res+ 'World')
+  .catch((e) => console.log('I am an error sadBois')) //catches errors up to current line
+  .then((res) => console.log(res))
+  .catch((e) => console.log('I am an error sadBois'));
 
 console.log('hi');
